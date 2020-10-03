30f355a0514906d127cf6c8a604e36527db537de
michael.yak <michael.yakobi@solaredge.com>
2020-10-03 15:24:31 +0300

    Tornady support should follow mypy & pylint conventions

--- pyctuator/impl/tornado_pyctuator.py
+++ pyctuator/impl/tornado_pyctuator.py
@@ -1,86 +1,129 @@
 import dataclasses
 import json
-from collections import defaultdict
 from datetime import datetime
 from functools import partial
-from typing import Any, Callable, List, Mapping
+from typing import Any, Optional
 
 from tornado.web import Application, RequestHandler
 
-from pyctuator.httptrace import TraceRecord, TraceRequest, TraceResponse
-from pyctuator.impl import SBA_V2_CONTENT_TYPE
 from pyctuator.impl.pyctuator_impl import PyctuatorImpl
 from pyctuator.impl.pyctuator_router import PyctuatorRouter
 
+
+# pylint: disable=abstract-method
 class AbstractPyctuatorHandler(RequestHandler):
-    pyctuator = None
-    dumps = None
-    def initialize(self):
-        self.pyctuator = self.application.settings.get('pyctuator')
+    pyctuator_router: Optional[PyctuatorRouter] = None
+    dumps: Optional[partial[str]] = None
+
+    def initialize(self) -> None:
+        self.pyctuator_router = self.application.settings.get('pyctuator_router')
         self.dumps = self.application.settings.get('custom_dumps')
 
+
 class PyctuatorHandler(AbstractPyctuatorHandler):
-    def get(self):
-        resp = self.pyctuator.get_endpoints_data()
+    def get(self) -> None:
+        assert self.pyctuator_router is not None
+        assert self.dumps is not None
+        resp = self.pyctuator_router.get_endpoints_data()
         self.write(self.dumps(resp))
+
 
 # GET /env
 class EnvHandler(AbstractPyctuatorHandler):
-    def options(self):
+    def options(self) -> None:
+        assert self.pyctuator_router is not None
+        assert self.dumps is not None
         self.write('')
-    def get(self):
-        resp = self.pyctuator.pyctuator_impl.get_environment()
+
+    def get(self) -> None:
+        assert self.pyctuator_router is not None
+        assert self.dumps is not None
+        resp = self.pyctuator_router.pyctuator_impl.get_environment()
         self.write(self.dumps(resp))
+
 
 # GET /info
 class InfoHandler(AbstractPyctuatorHandler):
-    def options(self):
+    def options(self) -> None:
+        assert self.pyctuator_router is not None
+        assert self.dumps is not None
         self.write('')
-    def get(self):
-        resp = self.pyctuator.pyctuator_impl.app_info
+
+    def get(self) -> None:
+        assert self.pyctuator_router is not None
+        assert self.dumps is not None
+        resp = self.pyctuator_router.pyctuator_impl.app_info
         self.write(self.dumps(resp))
+
 
 # GET /health
 class HealthHandler(AbstractPyctuatorHandler):
-    def options(self):
+    def options(self) -> None:
+        assert self.pyctuator_router is not None
+        assert self.dumps is not None
         self.write('')
-    def get(self):
-        resp = self.pyctuator.pyctuator_impl.get_health()
+
+    def get(self) -> None:
+        assert self.pyctuator_router is not None
+        assert self.dumps is not None
+        resp = self.pyctuator_router.pyctuator_impl.get_health()
         self.write(self.dumps(resp))
+
 
 # GET /metrics
 class MetricsHandler(AbstractPyctuatorHandler):
-    def options(self):
+    def options(self) -> None:
+        assert self.pyctuator_router is not None
+        assert self.dumps is not None
         self.write('')
-    def get(self):
-        resp = self.pyctuator.pyctuator_impl.get_metric_names()
+
+    def get(self) -> None:
+        assert self.pyctuator_router is not None
+        assert self.dumps is not None
+        resp = self.pyctuator_router.pyctuator_impl.get_metric_names()
         self.write(self.dumps(resp))
+
 
 # GET "/metrics/{metric_name}"
 class MetricsNameHandler(AbstractPyctuatorHandler):
-    def get(self, metric_name):
-        resp = self.pyctuator.pyctuator_impl.get_metric_measurement(metric_name)
+    def get(self, metric_name: str) -> None:
+        assert self.pyctuator_router is not None
+        assert self.dumps is not None
+        resp = self.pyctuator_router.pyctuator_impl.get_metric_measurement(metric_name)
         self.write(self.dumps(resp))
+
 
 # GET /loggers
 class LoggersHandler(AbstractPyctuatorHandler):
-    def options(self):
+    def options(self) -> None:
+        assert self.pyctuator_router is not None
+        assert self.dumps is not None
         self.write('')
-    def get(self):
-        resp = self.pyctuator.pyctuator_impl.logging.get_loggers()
+
+    def get(self) -> None:
+        assert self.pyctuator_router is not None
+        assert self.dumps is not None
+        resp = self.pyctuator_router.pyctuator_impl.logging.get_loggers()
         self.write(self.dumps(resp))
+
 
 # GET /loggers/{logger_name}
 # POST /loggers/{logger_name}
 class LoggersNameHandler(AbstractPyctuatorHandler):
-    def get(self, logger_name):
-        resp = self.pyctuator.pyctuator_impl.logging.get_logger(logger_name)
+    def get(self, logger_name: str) -> None:
+        assert self.pyctuator_router is not None
+        assert self.dumps is not None
+        resp = self.pyctuator_router.pyctuator_impl.logging.get_logger(logger_name)
         self.write(self.dumps(resp))
-    def post(self, logger_name):
-        body = self.request.body.decode('utf-8')
-        body = json.loads(body)
-        self.pyctuator.pyctuator_impl.logging.set_logger_level(logger_name, body.get('configuredLevel', None))
+
+    def post(self, logger_name: str) -> None:
+        assert self.pyctuator_router is not None
+        assert self.dumps is not None
+        body_str = self.request.body.decode('utf-8')
+        body = json.loads(body_str)
+        self.pyctuator_router.pyctuator_impl.logging.set_logger_level(logger_name, body.get('configuredLevel', None))
         self.write('')
+
 
 # pylint: disable=too-many-locals,unused-argument
 class TornadoHttpPyctuator(PyctuatorRouter):
@@ -91,7 +134,7 @@ class TornadoHttpPyctuator(PyctuatorRouter):
             json.dumps, default=self._custom_json_serializer
         )
 
-        app.settings.setdefault("pyctuator", self)
+        app.settings.setdefault("pyctuator_router", self)
         app.settings.setdefault("custom_dumps", custom_dumps)
         app.add_handlers(".*$", [
             (r"/pyctuator", PyctuatorHandler),
